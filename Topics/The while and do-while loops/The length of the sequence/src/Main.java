e19d5d14e4123e3e60c9265c4cedefad712a93e5
F1iper <ftimofiejew@gmail.com>
2022-08-31 23:13:37 +0200

    The length of sequence

---
+++ Topics/The while and do-while loops/The length of the sequence/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int counter = 0;
+        
+        while(true) {
+            int number = scanner.nextInt();
+            
+            if (number == 0) {
+                break;
+            }
+            counter++;
+        }
+        System.out.println(counter);
+    }
+}
